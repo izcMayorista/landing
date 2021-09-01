ea1428ce724bd07ab91bd9b8eb5caf13dce4cfdf
Diego Gonzáĺez <diegoandresgonzalezdiaz@gmail.com>
2021-09-01 08:03:01 -0500

    Cambios texto UPS

--- IZC/UPS/js/main.js
+++ IZC/UPS/js/main.js
@@ -18,18 +18,10 @@ const buttonContact = document.querySelector('.button_Contact');
 
 
 function showModalContact(){
-    // Swal.fire({
-    //     title: 'Contactanos',
-        
-    //     html: 
-    //         '<a href="https://wa.link/khjd49" class="btn_wp_sweet" target="_blank" rel="noopener"><img src="img/logo-whatsapp.png" alt="contacto por whatsapp"></a><a href="https://wa.link/khjd49" class="btn_wp_sweet" target="_blank" rel="noopener"><img src="img/logo-whatsapp.png" alt="contacto por whatsapp"></a>',
-    //     showCloseButton: true
-    // })
     Swal.fire({
         title: 'Contactanos por',
         html:
-            '<a href="https://wa.link/khjd49" class="btn_wp_sweet" target="_blank" rel="noopener"><img src="img/logo-whatsapp.png" alt="contacto por whatsapp"></a>'+
-            
+            '<a href="https://api.whatsapp.com/send?phone=+3118033847&text=Buen%20d%C3%ADa,%20me%20gustar%C3%ADa%20recibir%20m%C3%A1s%20informaci%C3%B3n%20de%20toda%20su%20l%C3%ADnea%20de%20UPS" class="btn_wp_sweet" target="_blank" rel="noopener"><img src="img/logo-whatsapp.png" alt="contacto por whatsapp"></a>'+
             '<a href="https://forms.gle/FQWtuAQY27mEZsY78" class="btn_wp_sweet" target="_blank" rel="noopener"><img src="img/logo-email.png" alt="contacto por email "></a>',
         showCloseButton: true,
         focusConfirm: false
